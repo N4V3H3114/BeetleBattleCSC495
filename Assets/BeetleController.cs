720c7a6dcc9868a85cc948cf3438b9828dc7ee85
N4v3N3114 <evanallen98@comcast.net>
2019-05-20 18:05:08 -0500

    Laying more groundwork for networking. Need to get bullets to shoot

--- Assets/BeetleController.cs
+++ Assets/BeetleController.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
 
-public class BeetleController : MonoBehaviour
+public class BeetleController : NetworkBehaviour
 {
     private Rigidbody rb;
     private Animation anim;
+    //Drag in the Bullet Emitter from the Component Inspector.
+    public GameObject Bullet_Emitter;
+
+    //Drag in the Bullet Prefab from the Component Inspector.
+    public GameObject Bullet;
+
+    public float Bullet_Forward_Force;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +25,11 @@ public class BeetleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         if (anim.IsPlaying("Shoot"))
         {
             float x = 0;
@@ -50,9 +63,27 @@ public class BeetleController : MonoBehaviour
       
     }
 
-    public void FireAnim()
+    [Command]
+    public void CmdFireAnim()
     {
-
+        Debug.Log("It should be shooting");
         anim.Play("Shoot");
+        //The Bullet instantiation happens here.
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+        //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+        //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+        //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+        Temporary_Bullet_Handler.GetComponent<Rigidbody>().velocity = Bullet.transform.forward * Bullet_Forward_Force;
+        
+        //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+       
+        NetworkServer.Spawn(Temporary_Bullet_Handler);
+        //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+        Destroy(Temporary_Bullet_Handler, 4.0f);
+
     }
 }
