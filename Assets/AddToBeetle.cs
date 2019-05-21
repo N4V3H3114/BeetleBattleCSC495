194140aad33372ed79fa0be5d46cbdd336f478bb
N4v3N3114 <evanallen98@comcast.net>
2019-05-21 12:40:08 -0500

    Pushing changes that have allowed a bullet to fire, just with almost no velocity

---
+++ Assets/AddToBeetle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToBeetle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject beetle = GameObject.FindGameObjectWithTag("Player");
+        transform.SetParent(beetle.transform, false);
+    }
+
+}
--- Assets/BeetleController.cs
+++ Assets/BeetleController.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
 
-public class BeetleController : MonoBehaviour
+public class BeetleController : NetworkBehaviour
 {
     private Rigidbody rb;
     private Animation anim;
+    //Drag in the Bullet Emitter from the Component Inspector.
+    public GameObject Bullet_Emitter;
+
+    //Drag in the Bullet Prefab from the Component Inspector.
+    public GameObject Bullet;
     // Start is called before the first frame update
+    public float Bullet_Forward_Force;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animation>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         if (anim.IsPlaying("Shoot"))
         {
             float x = 0;
@@ -50,9 +63,28 @@ public class BeetleController : MonoBehaviour
       
     }
 
-    public void FireAnim()
+    [Command]
+    public void CmdFireAnim()
     {
 
-        anim.Play("Shoot");
+        //The Bullet instantiation happens here.
+        Debug.Log("Should fire");
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+        //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+        //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+        //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+        Rigidbody Temporary_RigidBody;
+        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+        //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+        Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+        NetworkServer.Spawn(Temporary_Bullet_Handler);
+        //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+        Destroy(Temporary_Bullet_Handler, 4.0f);
     }
 }
